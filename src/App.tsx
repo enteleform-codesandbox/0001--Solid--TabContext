e69347a46cc286cfa8ee3b9185f9210c894671e1
Enteleform <enteleform@gmail.com>
2022-01-17 13:15:28 -0500

    ..

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import {Tab} from "./Tab"
+
+
+export function App(){
+	return (
+		<Tab.Container active="A">
+
+			<Tab.List>
+				<Tab.Button id="A"/>
+				<Tab.Button id="B"/>
+			</Tab.List>
+
+			<Tab.Panels>
+				<Tab.Panel id="A">
+					{"Tab Content 'A'"}
+				</Tab.Panel>
+
+				<Tab.Panel id="B">
+					{"Tab Content 'B'"}
+				</Tab.Panel>
+			</Tab.Panels>
+
+		</Tab.Container>
+	)
+}
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import Solid from "vite-plugin-solid"
+import {UserConfig} from "vite"
+
+
+export default {
+	plugins: [
+		Solid(),
+	],
+} as UserConfig
